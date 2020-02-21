1731410801d15a718fd7cee8bbd1854bb9122180
Fekete Márk <fmark98@gmail.com>
2020-02-21 10:34:49 +0100

    Point implementation.

--- Circle-Main/Circle-Main/Point.cpp
+++ Circle-Main/Circle-Main/Point.cpp
@@ -1,1 +1,21 @@
 #include "Point.h"
+
+double Point::getX() const
+{
+	return this->_x;
+}
+
+double Point::getY() const
+{
+	return this->_y;
+}
+
+void Point::setX(double x)
+{
+	this->_x = x;
+}
+
+void Point::setY(double y)
+{
+	this->_y = y;
+}
--- Circle-Main/Circle-Main/Point.h
+++ Circle-Main/Circle-Main/Point.h
@@ -1,11 +1,16 @@
 #pragma once
 class Point
 {
+private:
+	double _x = 0;
+	double _y = 0;
+
 public:
 	Point();
-
-private:
-	double x;
-	double y;
+	Point(double x, double y) : _x(x), _y(y) { }
+	double getX() const; // query
+	double getY() const; // query
+	void setX(double x);
+	void setY(double y);
 };
 
